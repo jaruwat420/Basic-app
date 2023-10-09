5a71117f4aeec1b104ce142815ff7f926190a51a
jaruwat420 <jaruwatcj@gmail.com>
2023-10-09 14:29:18 +0700

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const port = 3000;
+const app = express;
+
+
+const server = http.createServer((req, res) => {
+
+});
+
+server.listen(port , ()=>{
+    console.log(`Server Running on Port ${port}.`);
+});
