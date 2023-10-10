17d6751735470376ac83c6ef6677fd9f457c8a06
jaruwat420 <jaruwatcj@gmail.com>
2023-10-10 10:27:13 +0700

    add

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ import express from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import adminRoutes from "./routes/admin.routes.js";
-import util from "util";
+import { engine } from 'express-handlebars';
+
 
 const app = express();
 const port = 3000;
+
+// Hbs config 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 // Static file
 app.use(express.static("public"));
 
 // setting 
 app.set("port", process.env.PORT || 3000);
+app.set('view engine', 'hbs');
+
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,5 +2,5 @@ import {response} from "express";
 import {pool} from "../models/db.js";
 
 export const renderData = async (req, res) => {
-    res.render("index.hbs", {car_data: "Hello world....."});
+    res.render("index", {car_data: "Hello world....."});
 }
